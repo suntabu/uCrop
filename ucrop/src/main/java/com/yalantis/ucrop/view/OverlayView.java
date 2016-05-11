28ef41076e43aadac82ffebc64ce92c513db77e3
gouzhun <suntabu@163.com>
2016-05-11 20:24:30 +0800

    自定义形状crop

--- ucrop/src/main/java/com/yalantis/ucrop/view/OverlayView.java
+++ ucrop/src/main/java/com/yalantis/ucrop/view/OverlayView.java
@@ -29,7 +29,7 @@ public class OverlayView extends View {
 
     public static final boolean DEFAULT_SHOW_CROP_FRAME = true;
     public static final boolean DEFAULT_SHOW_CROP_GRID = true;
-    public static final boolean DEFAULT_OVAL_DIMMED_LAYER = false;
+    public static final boolean DEFAULT_OVAL_DIMMED_LAYER = true;
     public static final boolean DEFAULT_FREESTYLE_CROP_ENABLED = false;
     public static final int DEFAULT_CROP_GRID_ROW_COUNT = 2;
     public static final int DEFAULT_CROP_GRID_COLUMN_COUNT = 2;
@@ -222,7 +222,12 @@ public class OverlayView extends View {
 
         mGridPoints = null;
         mCircularPath.reset();
-        mCircularPath.addOval(mCropViewRect, Path.Direction.CW);
+//        mCircularPath.addOval(mCropViewRect, Path.Direction.CW);
+        mCircularPath.moveTo(mCropViewRect.centerX(), mCropViewRect.top);
+        mCircularPath.lineTo(mCropViewRect.right, mCropViewRect.centerY());
+        mCircularPath.lineTo(mCropViewRect.centerX(), mCropViewRect.bottom);
+        mCircularPath.lineTo(mCropViewRect.left, mCropViewRect.centerY());
+        mCircularPath.close();
     }
 
     protected void init() {
